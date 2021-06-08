0ae9de51dadd023ddad9b1e6a0a4c5da4973d54b
Lidiane Ivete da Silva <lidiane.ivete@gmail.com>
2021-06-08 12:56:55 -0300

    todavia no funciona con negativos

--- tp4ej2.py
+++ tp4ej2.py
@@ -1,10 +1,33 @@
-# FALTA: DEF
-# FALTA: CORREGIR PARA QUE FUNCIONE CON NEGATIVOS, ASÍ SOLO ANTA CON POSITIVOS
+################
+# Lidiane Ivete da Silva - @lidianeivete
+# TP4 - Ejercicio 2
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
 
-numero1 = int(input("ingrese el primer numero:"))
-numero2 = int(input("ingrese el segundo numero:"))
+#CORREGIR: TODAVIA NO FUNCIONA CON NEGATIVOS
 
-while numero1 != 0:
-    numero1 = numero1 - 1
-    numero2 = numero2 + 1
-    print (numero2)
+def suma_lenta (numero1, numero2):
+    while numero1 != 0:
+     numero1 = numero1 - 1
+     numero2 = numero2 + 1
+     return numero2
+
+def prueba():
+    numero1 = int(input("ingrese el primer numero:"))
+    numero2 = int(input("ingrese el segundo numero:"))
+    resultado = suma_lenta(numero1, numero2)
+    Print resultado
+
+if __name__ == "__main__":
+    prueba()
+    
+    
+# PRIMER INTENTO:
+#     
+# numero1 = int(input("ingrese el primer numero:"))
+# numero2 = int(input("ingrese el segundo numero:"))
+# 
+# while numero1 != 0:
+#     numero1 = numero1 - 1
+#     numero2 = numero2 + 1
+#     print (numero2)
