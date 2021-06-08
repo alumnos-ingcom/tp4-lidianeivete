a73819788c1299499c713047af243ec9169aee4e
Lidiane Ivete da Silva <lidiane.ivete@gmail.com>
2021-06-08 12:59:43 -0300

    este tambien CREO que está bien

--- tp4ej5.py
+++ tp4ej5.py
@@ -1,10 +1,33 @@
-# FALTA: METERLO EN UN DEF
+################
+# Lidiane Ivete da Silva - @lidianeivete
+# TP4 - ejercicio 5
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
 
-ingreso = int(input("ingrese un numero: "))
-if ingreso == 0:
-    print ("el ingreso es igual a cero.")
+def signo (ingreso):
+    if ingreso == 0:
+    return ("el ingreso es igual a cero.")
 elif ingreso > 0:
-    print ("el ingreso es positivo.")
+    return "el ingreso es positivo."
 else:
-    print ("el ingreso es negativo.")
+    return "el ingreso es negativo."
+
+def prueba():
+    ingreso = int(input("ingrese un numero: "))
+    resultado = signo (ingreso)
+    print resultado
+    
+if __name__ == "__main__":
+    prueba()    
+
+
+# PRIMER INTENTO:
+#     
+# ingreso = int(input("ingrese un numero: "))
+# if ingreso == 0:
+#     print ("el ingreso es igual a cero.")
+# elif ingreso > 0:
+#     print ("el ingreso es positivo.")
+# else:
+#     print ("el ingreso es negativo.")
     
