4bd5ecef563fbe1762fc12d33923e26f0c693e7f
Lidiane Ivete da Silva <lidiane.ivete@gmail.com>
2021-06-08 13:01:04 -0300

    creo que ya está con este ej

--- tp4ej7.py
+++ tp4ej7.py
@@ -1,10 +1,32 @@
-# FALTA: METERLO EN UN DEF
+################
+# Lidiane Ivete da Silva - @lidianeivete
+# TP4 - ejercicio 7
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
 
-dividendo = int(input("ingrese el dividendo: "))
-divisor = int(input("ingrese el divisor: "))
-cociente = 0
-while dividendo > divisor:
-    dividendo = dividendo - divisor
-    cociente = cociente + 1
-print ("el cociente es: ", cociente)
-print ("el resto es: ", dividendo)
+def resta_sucesiva (dividendo,divisor):
+    cociente = 0
+    while dividendo > divisor:
+        dividendo = dividendo - divisor
+        cociente = cociente + 1
+    return "el cociente es: ", cociente, " y el resto es: ", dividendo
+    
+def prueba():
+    dividendo = int(input("ingrese el dividendo: "))
+    divisor = int(input("ingrese el divisor: "))
+    resultado = resta_sucesiva (dividendo,divisor)
+    print (resultado)
+
+if __name__ == "__main__":
+    prueba()
+
+#PRIMER INTENTO:
+# 
+# dividendo = int(input("ingrese el dividendo: "))
+# divisor = int(input("ingrese el divisor: "))
+# cociente = 0
+# while dividendo > divisor:
+#     dividendo = dividendo - divisor
+#     cociente = cociente + 1
+# print ("el cociente es: ", cociente)
+# print ("el resto es: ", dividendo)
