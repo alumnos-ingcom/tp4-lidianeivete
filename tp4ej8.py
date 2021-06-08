138c9e761a03ec4445fad99461e4b3a66cb33964
Lidiane Ivete da Silva <lidiane.ivete@gmail.com>
2021-06-08 13:03:32 -0300

    falta terminarlo

--- tp4ej8.py
+++ tp4ej8.py
@@ -1,3 +1,13 @@
+################
+# Lidiane Ivete da Silva - @lidianeivete
+# TP4 - ejercicio 8
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+# FALTA: METERLO EN UN DEF
+# FALTA: COMPARAR LOS ELEMENTOS Y ORDENARLOS, O SEA, FALTA TODO!!
+
 variable1 = 1
 variable2 = 2
 variable3 = 3
