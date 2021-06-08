49ab2bc71601de01e386efc22d780deff37a9ef7
Lidiane Ivete da Silva <lidiane.ivete@gmail.com>
2021-06-08 12:55:35 -0300

    todavia no funciona adentro de las funciones. puse en comentarios mis dudas.

--- tp4ej1.py
+++ tp4ej1.py
@@ -1,15 +1,54 @@
-# FALTA: ENTENDER COMO METER ESO ADENTRO DE UN DEF
+################
+# Lidiane Ivete da Silva - @lidianeivete
+# TP4 - Ejercicio 1
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
 
-for i in range (0,3):
+def ingreso_reintento (ingreso,veces=5):
+    
+    for i in range (0,veces):
     
       try:
-                ingreso = int(input ("ingrese un numero intero:"))
-                break
+          ingreso = int(ingreso) #ACA DEVERÍA VOLVER A PEDIR EL NUMERO, PERO NO SÉ COMO HACERLO
+          break # ACA ME QUEDA LA DUDA SI USO BREAK O PASS
       except ValueError:
-                print ("La entrada no es un numero entero.")
-print ("Lo siento, se agotaron los intentos!")                
+          print ("La entrada no es un numero entero.")
+                                 
+    return "Lo siento, se agotaron los intentos!" 
 
+def prueba():
+    ingreso = int(input ("ingrese un numero intero:"))
+    resultado = ingreso_reintento
+    print (resultado)
+    
+def ingreso_limitado (ingreso, minimo=1, maximo=10):
+    
+    if ingreso_entero < maximo and ingreso_entero > minimo :
+      return "correcto!"
+    else:
+      return "El ingreso no está entre los numeros ", minimo," y ", maximo  
+    
+    
+def prueba():    
+    ingreso = input ("ingrese un numero entero:")
+    resultado = ingreso_limitado
+    print (resultado)
 
+if __name__ == "__main__":
+    prueba()
+    
+#
+# PRIMER INTENTO:
+# 
+# for i in range (0,3):
+# 
+#       try:
+#                 ingreso = int(input ("ingrese un numero intero:"))
+#                 break
+#       except ValueError:
+#                 print ("La entrada no es un numero entero.")
+# print ("Lo siento, se agotaron los intentos!") 
+# 
 #                 
 # ingreso_entero = int(input("ingrese un numero entre 1 y 10:"))
 #    
@@ -17,8 +56,8 @@ print ("Lo siento, se agotaron los intentos!")
 #   print ("correcto!")
 # else:
 #   print ("El ingreso no está entre los numeros 1 y 10.")
-                
-                
+#                 
+#                 
                     
                                         
                
