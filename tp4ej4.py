43dfd81bc108e1ea99ea82d158a6a7afd2626459
Lidiane Ivete da Silva <lidiane.ivete@gmail.com>
2021-06-08 12:58:48 -0300

    uno de los pocos que creo que está bien, solo creo!

--- tp4ej4.py
+++ tp4ej4.py
@@ -1,8 +1,35 @@
-numero1 = int(input("ingrese un numero: "))
-numero2 = int(input("ingrese otro numero: "))
-if numero1 == numero2:
-    print ("0")
+################
+# Lidiane Ivete da Silva - @lidianeivete
+# TP4 - ejercicio 4
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+def compara_numeros(numero1, numero2):
+    if numero1 == numero2:
+    return 0
 elif numero1 > numero2:
-    print ("1")
+    return 1
 else:
-    print ("-1")
+    return -1
+    
+    
+def prueba():
+    numero1 = int(input("ingrese un numero: "))
+    numero2 = int(input("ingrese otro numero: "))
+    resultado = compara_numeros (numero1, numero2)
+    print resultado
+
+if __name__ == "__main__":
+    prueba()
+
+
+# PRIMER INTENTO:
+
+# numero1 = int(input("ingrese un numero: "))
+# numero2 = int(input("ingrese otro numero: "))
+# if numero1 == numero2:
+#     print (0)
+# elif numero1 > numero2:
+#     print (1)
+# else:
+#     print (-1)
